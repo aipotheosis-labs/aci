bfc1319da771777cc59b2547708174ba136987ad
Patrick Zietkiewicz <patrick.z@aipolabs.xyz>
2025-01-20 21:33:39 +0000

    style: removed comments

--- aipolabs/common/db/crud/apps.py
+++ aipolabs/common/db/crud/apps.py
@@ -126,23 +126,3 @@ def set_app_active_status(db_session: Session, app_id: UUID, active: bool) -> No
 def set_app_visibility(db_session: Session, app_id: UUID, visibility: Visibility) -> None:
     statement = update(App).filter_by(id=app_id).values(visibility=visibility)
     db_session.execute(statement)
-
-
-# def filter_apps_by_configuration(
-#     db_session: Session,
-#     project_id: UUID,
-#     apps: list[tuple[App, float | None]],
-# ) -> list[tuple[App, float | None]]:
-#     """Filter apps list to only include apps that have configurations for this project."""
-#     app_ids = [app.id for app, _ in apps]
-#     statement = (
-#         select(App, AppConfiguration)
-#         .join(AppConfiguration)
-#         .filter(AppConfiguration.project_id == project_id, App.id.in_(app_ids))
-#     )
-#     configured_app_ids = {row[0].id for row in db_session.execute(statement)}
-
-#     # Filter original list to preserve ordering and scores
-#     configured_apps = [(app, score) for app, score in apps if app.id in configured_app_ids]
-
-#     return configured_apps
