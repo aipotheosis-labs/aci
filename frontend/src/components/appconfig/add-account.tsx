b6736d74a76d6e7f3b4f89f9afc2b767c358fa02
tianlilei-c <2816999029@qq.com>
2025-05-01 18:33:46 +0800

    feat: support full app logo loading in AddAccountForm

--- frontend/src/components/appconfig/add-account.tsx
+++ frontend/src/components/appconfig/add-account.tsx
@@ -46,6 +46,7 @@ import {
 } from "@/lib/api/linkedaccount";
 import { getApiKey } from "@/lib/api/util";
 import { useMetaInfo } from "@/components/context/metainfo";
+import Image from "next/image";
 
 const formSchema = z
   .object({
@@ -76,6 +77,7 @@ const FORM_SUBMIT_API_KEY = "apiKey";
 const FORM_SUBMIT_NO_AUTH = "noAuth";
 export interface AppInfo {
   name: string;
+  logo: string | undefined;
   securitySchemes: string[];
 }
 interface AddAccountProps {
@@ -336,7 +338,17 @@ export function AddAccountForm({
                     <SelectContent>
                       {appInfos.map((appInfo) => (
                         <SelectItem key={appInfo.name} value={appInfo.name}>
-                          {appInfo.name}
+                          <div className="flex items-center gap-2">
+                            {appInfo.logo && (
+                              <Image
+                                src={appInfo.logo}
+                                alt={appInfo.name}
+                                width={16}
+                                height={16}
+                              />
+                            )}
+                            {appInfo.name}
+                          </div>
                         </SelectItem>
                       ))}
                     </SelectContent>
