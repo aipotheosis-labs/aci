72b245b2c3614841a3b9ec36bebe85fd3c1bbd0f
fixer <14433035+thisisfixer@users.noreply.github.com>
2025-06-17 10:41:15 +0100

    delete unused functions

--- backend/aci/server/billing.py
+++ backend/aci/server/billing.py
@@ -3,28 +3,11 @@ from uuid import UUID
 from sqlalchemy.orm import Session
 
 from aci.common.db import crud
-from aci.common.db.sql_models import Plan, Project, Subscription
-from aci.common.enums import StripeSubscriptionStatus
+from aci.common.db.sql_models import Plan, Project
 from aci.common.exceptions import MonthlyQuotaExceeded, SubscriptionPlanNotFound
 from aci.common.logging_setup import get_logger
-from aci.common.schemas.subscription import SubscriptionPublic
 
 logger = get_logger(__name__)
-
-
-def get_subscription_and_plan_by_org_id(
-    db_session: Session, org_id: UUID
-) -> tuple[Subscription | None, Plan]:
-    subscription = crud.subscriptions.get_subscription_by_org_id(db_session, org_id)
-    plan = None
-    if not subscription:
-        plan = crud.plans.get_by_name(db_session, "free")
-    else:
-        plan = crud.plans.get_by_id(db_session, subscription.plan_id)
-
-    if not plan:
-        raise SubscriptionPlanNotFound("Plan not found")
-    return subscription, plan
 
 
 def get_active_plan_by_org_id(db_session: Session, org_id: UUID) -> Plan:
@@ -33,31 +16,10 @@ def get_active_plan_by_org_id(db_session: Session, org_id: UUID) -> Plan:
         active_plan = crud.plans.get_by_name(db_session, "free")
     else:
         active_plan = crud.plans.get_by_id(db_session, subscription.plan_id)
+
     if not active_plan:
         raise SubscriptionPlanNotFound("Plan not found")
     return active_plan
-
-
-def get_subscription_by_org_id(db_session: Session, org_id: UUID) -> SubscriptionPublic:
-    subscription = crud.subscriptions.get_subscription_by_org_id(db_session, org_id)
-    if not subscription:
-        # If no subscription found, use the free plan
-        plan = crud.plans.get_by_name(db_session, "free")
-        if not plan:
-            raise SubscriptionPlanNotFound("Free plan not found")
-        return SubscriptionPublic(
-            plan=plan.name,
-            status=StripeSubscriptionStatus.ACTIVE,
-        )
-
-    # Get the plan from the subscription
-    plan = crud.plans.get_by_id(db_session, subscription.plan_id)
-    if not plan:
-        raise SubscriptionPlanNotFound(f"Plan {subscription.plan_id} not found")
-    return SubscriptionPublic(
-        plan=plan.name,
-        status=subscription.status,
-    )
 
 
 def increment_quota(db_session: Session, project: Project, monthly_quota_limit: int) -> None:
